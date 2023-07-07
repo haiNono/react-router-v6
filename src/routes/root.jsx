21e547fb609207ce7bf0259b2488130ef7e63014
hainuo.li <hainuo.li@transwarp.io>
2023-07-07 14:40:38 +0800

    feat: navLink

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,10 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from "react-router-dom";
+import {
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+} from "react-router-dom";
 import { getContacts, createContact } from "../contact";
 
 export default function root() {
@@ -28,7 +34,12 @@ export default function root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -37,7 +48,7 @@ export default function root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
