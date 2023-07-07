51a02940db72f5fffd173842b32ad31eff269bb0
hainuo.li <hainuo.li@transwarp.io>
2023-07-07 16:39:31 +0800

    feat: 两个问题  刷新和回退

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,28 +6,33 @@ import {
   redirect,
   useNavigation,
 } from "react-router-dom";
+import { useEffect } from "react";
 import { getContacts, createContact } from "../contact";
 
 export default function root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
@@ -74,9 +79,11 @@ export default function root() {
   );
 }
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export async function action() {
